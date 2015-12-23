05bb3c9a52c6965a7d294de8c3f856cf21ab387e
lauri <lauri.schnabel05@gmail.com>
2015-12-23 16:09:29 +0100

    .

--- javascript/httpserver.js
+++ javascript/httpserver.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
   response.write('<title>Lauri\' Seite</title>');
   response.write('</head>');
   response.write('<body>');
-  response.write('Hallo lieber Lauri, wie geht es Dir?');
+  response.write('Hallo lieber Lauri, wie geht es dir?');
   response.write('</body>');
   response.write('</html>');
   response.end();
