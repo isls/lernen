4568c2faa8b7da19a82c708bdef53df6c0292cb5
insc <ingo.schnabel@gmail.com>
2018-01-27 23:23:25 +0100

    .

--- javascript/003_sort.js
+++ javascript/003_sort.js
@@ -1,24 +1,21 @@
+var list = [1, 2, 3];
 
+console.log('List: ' + list);
+console.log('Länge der List: ' + list.length);
+console.log('Zufallszahl : ' + calcRandomInt(0, 100));
+console.log('****Neue Zufallszahlen zur Liste hinzufügen****');
+list.push(calcRandomInt(0, 100));
+list.push(calcRandomInt(0, 100));
+list.push(calcRandomInt(0, 100));
+list.push(calcRandomInt(0, 100));
+list.push(calcRandomInt(0, 100));
+list.push(calcRandomInt(0, 100));
+list.push(calcRandomInt(0, 100));
 
-var list = new Array(1, 2, 3);
+console.log('List: ' + list);
+console.log('Länge der List: ' + list.length);
+console.log('Zufallszahl : ' + calcRandomInt(0, 100));
 
-console.log("List: " + list);
-console.log("Länge der List: " + list.length);
-console.log("Zufallszahl : " + random(0,100));
-console.log("****Neue Zufallszahlen zur Liste hinzufügen****")
-list.push(random(0,100));
-list.push(random(0,100));
-list.push(random(0,100));
-list.push(random(0,100));
-list.push(random(0,100));
-list.push(random(0,100));
-list.push(random(0,100));
-console.log("List: " + list);
-console.log("Länge der List: " + list.length);
-console.log("Zufallszahl : " + random(0,100));
-
-
-
-function random (min, max) {
-    return  Math.round(Math.random() * (max - min) + min);
+function calcRandomInt (min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
